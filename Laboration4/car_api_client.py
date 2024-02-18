d667404cc2f83714fe2678dfe2759b739954e397
Isak Lagerberg <isalag-1@student.ltu.se>
2024-02-18 15:47:56 +0100

    Updatera buggar

--- Laboration4/car_api_client.py
+++ Laboration4/car_api_client.py
@@ -1,30 +1,31 @@
+import os
 import flask_app 
 import json
 
 # Create a test client to send requests
 client = flask_app.app.test_client()
 
-def get_main_page():
+def get_main_page_req():
     response = client.get('/api/v1/home')
     print(response.text)
 
-def get_cars_jsonlist():
+def get_cars_jsonlist_req():
     response = client.get('/api/v1/cars')
     data = json.loads(response.text)
     cars = data['cars']
     for car in cars:
         print("\nCar info:")
-        for key, value in car.items():
-            print(f"{key}: {value}")
+        for key in car:
+            print(f"{key}: {car[key]}")
 
-def get_car_by_registration_number(registration_number):
+def get_car_by_registration_number_req(registration_number):
     response = client.get(f'/api/v1/cars/{registration_number}')
     data = json.loads(response.text)
-    print("\nCar info: ")
+    print("\nCar info:")
     for key, value in data["car"].items():
         print(f"{key}: {value}")
 
-def get_car_by_owner(owner):
+def get_car_by_owner_req(owner):
     response = client.get(f'/api/v1/cars/{owner}')
     data = json.loads(response.text)
     print("\nCar info: ")
@@ -32,7 +33,7 @@ def get_car_by_owner(owner):
         print(f"{key}: {value}")
 
 # Send a POST request
-def add_car(registration_number, owner, model):
+def add_car_req(registration_number, owner, model):
     post_response = client.post(f'/api/v1/cars/{registration_number}', json={
         'registration number': f'{registration_number}',
         'owner': f'{owner}',
@@ -41,23 +42,25 @@ def add_car(registration_number, owner, model):
     print(post_response.get_json())
 
 # Send a DELETE request
-def delete_car(registration_number):
+def delete_car_req(registration_number):
     delete_response = client.delete(f'/api/v1/cars/{registration_number}')
     print(delete_response.get_json())
 
-def update_owner(registration_number, new_owner):
+def update_owner_req(registration_number, new_owner):
     put_response = client.put(f'/api/v1/cars/{registration_number}', json={'new_owner': f'{new_owner}'})
     print(put_response.get_json())
 
 
+os.system('cls' if os.name == 'nt' else 'clear')
+
 #get_main_page()
 
-get_cars_jsonlist()
+get_cars_jsonlist_req() # Soterars i alfabetisk ordning pga items
 
 #get_car_by_registration_number("THW297")
 
-#add_car("ABC123", "John Doe", "Toyota Corolla")
+#add_car_req("ABC123", "John Doe", "Toyota Corolla")
 
-#delete_car("THW297")
+#delete_car_req("THW297")
 
 #update_owner("ABC123", "Jane Doe")
--- Laboration4/flask_app.py
+++ Laboration4/flask_app.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 @app.route('/api/v1/home')
 def main():
-    welcome_message = 'Välkommen till Bilfirman Ruffel och Båg. Just nu befinner du dig /api/v1/home. Nedan finns samtliga tillgänliga sökvägar listade inklusive deras funktion:\n'
+    welcome_message = 'Välkommen till Bilfirman Ruffel och Båg. Just nu befinner du dig på sökvägen /api/v1/home. \nNedan finns samtliga tillgänliga sökvägar listade inklusive deras funktion:\n'
     get_cars = '/api/v1/cars - Hämtar en jsondict innehållande alla bilar och deras info\n'
     reg_get = '/api/v1/cars/<registration_number> [GET] - Hämtar en bils info baserat registreringsnummer\n'
     owner_get = '/api/v1/cars/<owner> [GET] - Hämtar en bils info baserat ägare\n'
@@ -50,7 +50,7 @@ def get_car_by_owner(owner):
     return jsonify({'Car owner not found!': car}), 404
 
 @app.route('/api/v1/cars/<registration_number>', methods=['POST'])
-def add_car():
+def add_car(registration_number):
     try:
         registration_number = request.json['registration number']
         owner = request.json['owner']
@@ -63,10 +63,10 @@ def add_car():
         if(car['registration number'] == registration_number):
             return jsonify({'Error': 'Registration number already exists!'}), 409
 
-    new_car = {'registration number': registration_number, 'model': model, 'owner': owner}
+    new_car = {'registration number': registration_number, 'owner': owner, 'model': model}
     cars.append(new_car)
 
-    with open(filepath, 'w', 'utf-8-sig') as json_file:
+    with open(filepath, 'w', encoding='utf-8-sig') as json_file:
         json.dump(cars, json_file, indent=2)
 
     return jsonify({'Message': 'Registration number added successfully!'}), 200
@@ -76,10 +76,10 @@ def delete_car(registration_number):
     for car in cars:
         if car['registration number'] == registration_number:
             cars.remove(car)
-            with open(filepath, 'w', 'utf-8-sig') as json_file:
+            with open(filepath, 'w', encoding= 'utf-8-sig') as json_file:
                 json.dump(cars, json_file, indent=2)
             return jsonify({'Message': 'Registration number and coherent data deleted successfully!'}), 200
-        return jsonify({'Error': 'Registration number not found!'}), 404
+    return jsonify({'Error': 'Registration number not found!'}), 404
 
 @app.route('/api/v1/cars/<registration_number>', methods=['PUT'])
 def update_owner(registration_number):
